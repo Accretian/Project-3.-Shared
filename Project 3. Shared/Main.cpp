625440b31d656c12648a5fd8411469dff08ac1d5
Accretian <kvik.ocean@gmail.com>
2025-04-05 23:01:07 +0300

    1

---
+++ Project 3. Shared/Main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+
+	std::string pleaseDontCloseThisProgram;
+
+	std::vector<int> vector1(100);
+	for (int i = 0; i < 100; i++) {
+		vector1[i] = i + 1;
+	}
+
+	for (int i = 1; i <= vector1.size(); i++) {
+		std::cout << vector1[i - 1] << ".";
+		if (i % 10 == 0) {
+			std::cout << "\n";
+		}
+	}
+
+
+	std::cin >> pleaseDontCloseThisProgram;
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
