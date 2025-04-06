2ab9753a1e7e30281dc28619958d3d2b8fae3213
Accretian <kvik.ocean@gmail.com>
2025-04-06 19:57:32 +0300

    1

--- Project 3. Shared/Main.cpp
+++ Project 3. Shared/Main.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stdio.h>
 
 int main() {
 
 	std::string pleaseDontCloseThisProgram;
 
-	std::vector<int> vector1(100);
-	for (int i = 0; i < 100; i++) {
+	std::vector<int> vector1(100000);
+	for (int i = 0; i < vector1.size(); i++) {
 		vector1[i] = i + 1;
 	}
 
 	for (int i = 1; i <= vector1.size(); i++) {
-		std::cout << vector1[i - 1] << ".";
+		std::printf("%d", vector1[i - 1]);
+		std::printf(".");
 		if (i % 10 == 0) {
-			std::cout << "\n";
+			std::printf("\n");
 		}
 	}
 
 	std::cout << "\n\n\n";
 
+	
 
-	
-	
 
 
 
